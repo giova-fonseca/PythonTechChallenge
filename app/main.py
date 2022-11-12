03f659635c2a0bf844e54e00d92bb18be560c285
Giovanni Fonseca Mejia <gfonseca.cr@gmail.com>
2022-11-11 21:44:37 -0600

    added in the method bulid_stats information about less and greater information

--- app/main.py
+++ app/main.py
@@ -3,12 +3,21 @@ MAX_VALUE = 1000
 class DataCapture():
     def __init__(self):
         self.data = []
-    
+        self.mySet = set()
+            
     def add(self,value:int):
-        try:
-            if value and int(value) and value <= MAX_VALUE:
-                self.data.append(value)
-                self.data.sort()
+        try:           
+            if value:
+                if type(value) ==int:
+                    if value <= MAX_VALUE:                    
+                        self.data.append(value)
+                        self.data.sort()
+                    else:
+                        raise ValueError("The value must be less than 1000")
+                        
+                else:
+                    raise TypeError("The value must be Integer type")
+                                        
         except ValueError as e:
             print(f"Error executing method add for the value {value} {e.__str__()}")            
         except Exception as e:
@@ -17,9 +26,22 @@ class DataCapture():
     
     
     def build_stats(self,value:int):
-        print(self.data.index(value))
-        return len(self.data)
-
+        try:
+            if type(value) == int:
+                self.mySet.update(self.data)
+                print(f"Founded in the position {self.data.index(value)}")
+                print(f"Less than {value} are {self.data.index(value)}")
+                print(f"Greater than {value} are {len(self.data)-self.data.index(value)-1}")
+                print(f"Counts of items in the set {len(self.mySet)}")
+                print(f"Items in the set {self.mySet}")
+                print(f"Counts of items in the stats {len(self.data)}")
+                return self.mySet
+            else:
+                raise TypeError("The value must be Integer")
+        except TypeError as ex:
+            print(ex)
+        except Exception as ex:
+            print(ex)
     
     def less(self,value:int):
         pass
@@ -29,10 +51,11 @@ class DataCapture():
     
 capture = DataCapture()
 capture.add(value=4)
-capture.add(value=8)
+capture.add(value=3)
+capture.add(value=2)
+capture.add(value=5)
 capture.add(value=7)
 capture.add(value=3)
 stats = capture.build_stats(4)
-
 print(capture.__str__())
 print(stats)
